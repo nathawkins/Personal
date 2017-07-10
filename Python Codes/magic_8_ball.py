d0968a4c3a2458acafe321d1cd3289f513e3afb9
Nat Hawkins <nathawkins516@gmail.com>
2017-07-10 15:39:36 -0400

    Added magic 8 ball program

---
+++ Python Codes/magic_8_ball.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+import os
+import random
+
+## Functions
+def clear():
+    '''Clear the terminal window.'''
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+def greeting():
+    '''Creates the pop-up message at the top'''
+    clear()
+    message = "Shake the magic 8 ball and get the answers you seek!"
+    print('='*(len(message)+4))
+    print('= '+message+' =')
+    print('='*(len(message)+4))
+    print('\n\n')
+
+## Application Body
+greeting()
+if input("Ask your question and type `shake`, then hit `return`..."):
+    complete = 0
+    while complete == 0:
+        greeting()
+        responses = ["It is certain","It is decidedly so",'Without a doubt','Yes definitely','You may rely on it','As I see it, yes','Most likely','Outlook good','Yes','Signs point to yes','Reply hazy, try again','Ask again later','Better not tell you now','Cannot predict now','Concentrate harder and ask again',"Don't count on it",'My reply is no','My sources say no','Outlook not so good','Very doubtful']
+        shake = random.randint(0,len(responses))
+        print(responses[shake])
+        if input("Do you seek more answers? [y/n]") == 'n':
+            goodbye = "May your roads lead you to warm sands..."
+            print('-'*len(goodbye))
+            print(goodbye)
+            print('-'*len(goodbye))
+            print('\n\n')
+            complete = 1
--- README.md
+++ README.md
@@ -10,6 +10,7 @@ This is my personal repository. This will include any files that require mass sh
 
 1. Python code for text parsing
 1. Terminal application for writing a journal
+1. Terminal application for a magic 8 ball
 1. Cheatsheet for writing in Markdown
 1. Mathematica notebook for using hydrogen wave functions and spherical harmonics easier
 
