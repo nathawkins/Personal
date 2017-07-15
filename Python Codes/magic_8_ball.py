53bab3a11cf08deeaf12f670b1e6a9b10440c145
Nat Hawkins <nathawkins516@gmail.com>
2017-07-15 00:39:24 -0400

    Fixed bug in magic 8 ball

--- Python Codes/magic_8_ball.py
+++ Python Codes/magic_8_ball.py
@@ -23,7 +23,7 @@ if input("Ask your question and type `shake`, then hit `return`..."):
     while complete == 0:
         greeting()
         responses = ["It is certain","It is decidedly so",'Without a doubt','Yes definitely','You may rely on it','As I see it, yes','Most likely','Outlook good','Yes','Signs point to yes','Reply hazy, try again','Ask again later','Better not tell you now','Cannot predict now','Concentrate harder and ask again',"Don't count on it",'My reply is no','My sources say no','Outlook not so good','Very doubtful']
-        shake = random.randint(0,len(responses))
+        shake = random.randint(0,len(responses)-1)
         print(responses[shake])
         if input("Do you seek more answers? [y/n]") == 'n':
             goodbye = "May your roads lead you to warm sands..."
