a9e0805fb3eeb5c97f5d9e68ded91de4ddcb3713
Nat Hawkins <nathawkins516@gmail.com>
2018-07-09 10:11:39 -0400

    revising the script to make empty project repository

--- make_project_directory.sh
+++ make_project_directory.sh
@@ -1,7 +1,19 @@
 mkdir project_directory
 cd project_directory/
+touch README.md
+touch requirements.txt
+touch CITATION
+touch LICENSE
 mkdir data
-mkdir develop
-mkdir deliverable
+mkdir doc
+mkdir bin
 mkdir src
-mkdir figures
+mkdir results
+
+for file in */
+do
+    cd $file
+    touch README.md
+    cd ..
+done
+
